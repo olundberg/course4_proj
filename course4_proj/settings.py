8c0939888c23d89e6bba037bb3d348ac4c7f51c6
Oscar Lundberg <lundberg.oscar@gmail.com>
2022-02-08 09:38:23 +0000

    Finish django signals

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -17,8 +17,8 @@ import os
 class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
     BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    ADMINS = [("Ben", "ben@example.com")]
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
